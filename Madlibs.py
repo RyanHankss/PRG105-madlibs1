554279fc33582de234d28fb186314a71d74be8ca
RyanHankss <rhanks@mchenry.edu>
2016-09-01 15:25:51 -0500

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,32 @@
+#variables
+
+animal = "Hammerhead shark"
+place = "house"
+title = "Duke"
+pronoun = "we"
+verb1 = "Dance"
+verb2 = "Fighting"
+food = "egg rolls"
+command = "Begin"
+
+
+#poem Kookaburra Nursery Rhyme
+
+print(animal + " sits in the old gum " + place + ",")
+print("Merry, merry " + title + " of the bush is " + pronoun +  ".\n")
+
+print( verb1 + ", " + animal +",")
+
+
+print( verb1 + ", " + animal +",\n")
+print("Gay your life must be.\n")
+
+print(animal + " sits in the old gum " + place + ",")
+print(verb2 + " all the " + food + " he can see.\n")
+
+
+print(command + ", " + animal + ",")
+
+
+print(command + ", " + animal + ",\n")
+print("Leave some there for " + pronoun + ".")
